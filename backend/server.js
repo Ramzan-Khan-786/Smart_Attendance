98600903b1a2511b040ed220105ccb91b3d81479
KHAN RAMZAN B. <ramzankhan786929@gmail.com>
2025-09-18 21:13:28 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: "50mb" }));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://smart-attendance-mu.vercel.app/",
     methods: ["GET", "POST", "PUT"],
   },
 });
